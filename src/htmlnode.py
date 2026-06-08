5181fabd648b8f26c7b9bb2bd08ae740decd4638
Andrei Deak <andrei.deak@icloud.com>
2026-06-08 13:37:16 +0200

    add leafnode type

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,6 +1,12 @@
 
 class HTMLNode:
-    def __init__(self, tag:str=None, value:str=None, children:list[HTMLNode]=None, props:dict[str,str]=None):
+    def __init__(
+            self, 
+            tag: str | None = None, 
+            value: str | None = None, 
+            children: list[HTMLNode] | None = None, 
+            props: dict[str,str] | None = None
+    ):
         self.tag = tag
         self.value = value
         self.children = children
@@ -12,15 +18,44 @@ class HTMLNode:
     def props_to_html(self):
         if self.props is None:
             return ""
-        return f" href=\"{self.props["href"]}\" target=\"{self.props["target"]}\""
+        
+        return "".join(
+            f' {key}="{value}"'
+            for key, value in self.props.items()
+        )
 
     def __repr__(self):
-        return f""""
+        return f"""
         DEBUG | HTMLNode.tag: {self.tag}
         DEBUG | HTMLNode.value: {self.value}
         DEBUG | HTMLNode.children: {self.children}
         DEBUG | HTMLNode.props: {self.props}
         """
 
+class LeafNode(HTMLNode):
+    def __init__(
+            self, 
+            tag : str | None,
+            value : str, 
+            props: dict[str,str] | None = None):
+        super().__init__(tag, value, None, props)
 
+    def to_html(self):
+        if self.value is None:
+            raise Exception(ValueError)
 
+        if self.tag is None:
+            return self.value
+        
+        if self.props is not None:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"""
+        DEBUG | HTMLNode.tag: {self.tag}
+        DEBUG | HTMLNode.value: {self.value}
+        DEBUG | HTMLNode.children: {self.children}
+        DEBUG | HTMLNode.props: {self.props}
+        """
---
+++ src/test_leafnode.py
@@ -0,0 +1,19 @@
+import unittest
+from htmlnode import HTMLNode,LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_create(self):
+        leaf_node = LeafNode("p", "This is a paragraph.")
+        self.assertEqual(leaf_node.tag,"p")
+        self.assertEqual(leaf_node.value, "This is a paragraph.")
+
+    def test_leaf_to_html(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_link(self):
+        node = LeafNode("a", "Boot.dev", {"href": "https://boot.dev", "target": "_blank"})
+        self.assertEqual(node.to_html(), f'<a href="https://boot.dev" target="_blank">Boot.dev</a>')
+
+if __name__ == "__main__":
+    unittest.main()
